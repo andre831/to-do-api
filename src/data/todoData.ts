d0fb1a027fe2a8ff1af6b75a67de3bb7c59e353a
Andre Andre Andre <andredupdm@gmail.com>
2022-11-22 22:07:13 -0300

    feat: created querys to save and delete the task

--- src/data/todoData.ts
+++ src/data/todoData.ts
@@ -1,7 +1,18 @@
 import { db as database } from '../infra/database';
+import { Task, NewTask } from '../types/Task';
 
 function getTask(id: string) {
   return database.query(`SELECT * FROM todo.tasks WHERE "user_id"=${id}`);
 }
 
-export { getTask };
+function saveTask(task: NewTask) {
+  return database.one(
+    `INSERT INTO todo.tasks ("user_id", title, description) VALUES (${task.user_id},'${task.title}' ,'${task.description}') returning *`
+  );
+}
+
+function deleteTask(id: number) {
+  return database.one(`DELETE FROM todo.tasks WHERE id=${id} returning *`);
+}
+
+export { getTask, saveTask, deleteTask };
