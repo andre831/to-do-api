bffe80de16953d574929d29b6750656ca2cce2d6
Andre Andre Andre <andredupdm@gmail.com>
2022-11-22 22:04:48 -0300

    feat: created module test to verify get method

--- src/test/todo.test.ts
+++ src/test/todo.test.ts
@@ -1,10 +1,28 @@
 import axios from 'axios';
+import crypto from 'crypto';
 
-test('primeiro teste', async function () {
-  const tasks = await axios({
+import * as services from '../service/todoService';
+
+const gen = function () {
+  return crypto.randomBytes(20).toString('hex');
+};
+
+test('GET ALL TASKS FROM USER: ', async function () {
+  //given
+  const task = await services.saveTask({
+    user_id: 1,
+    title: gen(),
+    description: gen()
+  });
+
+  //when
+  const response = await axios({
     url: 'http://localhost:3000/tasks/1',
     method: 'get'
   });
 
-  console.log(tasks.data);
+  //then
+  expect(response.data).toHaveLength(1);
+
+  await services.deleteTask(task.id);
 });
