3ee219c26f2e9f299bea692d63e3d42473f8e263
Andre Andre Andre <andredupdm@gmail.com>
2022-12-01 20:54:48 -0300

    feat: created test to get only task

--- src/test/todo.test.ts
+++ src/test/todo.test.ts
@@ -1,28 +1,54 @@
 import axios from 'axios';
 import crypto from 'crypto';
 
+import { Task } from '../types/Task';
+
 import * as services from '../service/todoService';
 
 const gen = function () {
   return crypto.randomBytes(20).toString('hex');
 };
 
-test('GET ALL TASKS FROM USER: ', async function () {
-  //given
+function request(url: string, method: string, data?: any) {
+  return axios({ url, method, data });
+}
+
+test('GET ALL TASKS FROM USER', async function () {
   const task = await services.saveTask({
     user_id: 1,
     title: gen(),
     description: gen()
   });
 
-  //when
   const response = await axios({
     url: 'http://localhost:3000/tasks/1',
     method: 'get'
   });
 
-  //then
   expect(response.data).toHaveLength(1);
 
   await services.deleteTask(task.id);
+});
+
+test('GET ONLY TASK FROM USER', async () => {
+  const userId = '1';
+
+  const task = {
+    user_id: userId,
+    title: gen(),
+    description: gen()
+  };
+
+  const postTask = await request(
+    `http://localhost:3000/tasks/${userId}`,
+    'post',
+    task
+  ).then((res) => res.data as Task);
+
+  const response = await request(
+    `http://localhost:3000/tasks/${userId}/${postTask.id}`,
+    'get'
+  ).then((res) => res.data as Task);
+
+  if (response.id) expect(response.id).toBe(postTask.id);
 });
