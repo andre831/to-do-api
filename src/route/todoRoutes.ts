e8c352544525af2a390fd7e14fa937525696a6b4
Andre Andre Andre <andredupdm@gmail.com>
2022-12-01 22:34:02 -0300

    feat: created method to to save and get a task

--- src/route/todoRoutes.ts
+++ src/route/todoRoutes.ts
@@ -6,19 +6,34 @@ import { Params } from '../types/Req';
 
 const router = express.Router();
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:userId', async (req, res) => {
   const params: Params = req.params;
 
-  const id = params.id;
+  const userId = params.userId;
 
-  const tasks = await service.getAllTasksFromUser(id);
+  const tasks = await service.getAllTasksFromUser(userId);
 
   res.json(tasks);
 });
-// router.get('/tasks/:id', async (req, res) => {});
+
+router.get('/tasks/:userId/:id', async (req, res) => {
+  const params: Params = req.params;
+
+  const id = params.id as string;
+  const userId = params.userId;
+
+  const onlyTask = await service.getOnlyTask(userId, id);
+
+  res.json(onlyTask);
+});
+
+router.post('/tasks/:userId', async (req, res) => {
+  const newTask = service.saveTask(req.body);
+
+  res.json(await newTask);
+});
 
 // router.post('/tasks', async (req, res) => {});
-// router.post('/tasks/:id', async (req, res) => {});
 
 // router.put('/tasks/:id', async (req, res) => {});
 
